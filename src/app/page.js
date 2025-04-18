465f772c86247fe49fc497b1bfc60a5c512c2b6a
karimberry <karimccw8100@gmail.com>
2025-04-18 18:52:58 +0500

    call login on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import FeaturesSection from "@/components/homeSections/vpn-feature";
 import Image from "next/image";
+import LoginForm from "./login/page";
+
 
 export default function Home() {
   return (
     <div>
-    <FeaturesSection></FeaturesSection>
-      
+     
+      <LoginForm></LoginForm>
     </div>
   );
 }
