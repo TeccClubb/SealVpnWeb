08489d68c186a4b7f87a556b5bf9e9d3dd1922d1
karim-lucky <karimccw8100@gmail.com>
2025-04-23 20:06:46 +0500

    pricing page

---
+++ src/app/pricing/page.js
@@ -0,0 +1,14 @@
+ 
+
+
+import FaqTestimonialSection from "@/components/pricingSection/frequnetQuestion"
+import PricingPlans from "@/components/pricingSection/pricingPlans"
+export default function pricing(){
+
+    return(
+        <div>
+            <PricingPlans></PricingPlans>
+            <FaqTestimonialSection></FaqTestimonialSection>
+        </div>
+    )
+}
