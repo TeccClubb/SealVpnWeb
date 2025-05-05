f747c16915f60352aa696dc8acee2072271067d8
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-05-05 18:41:02 +0500

    account-billing-history page designed

---
+++ src/app/Dashboard/page.js
@@ -0,0 +1,13 @@
+
+import Sidebar from "@/components/Sidebar";
+import BillingHistory from "./Acount-biling-history/page";
+
+
+export default function Dashboard() {
+  return (
+    <div>
+     
+     <BillingHistory></BillingHistory>
+    </div>
+  );
+}
