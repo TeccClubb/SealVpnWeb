6e524589e0a7c0482f1573320b12915a34f3a09c
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-05-06 16:41:02 +0500

    now upadte and completed all dashboard

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,27 +7,26 @@ const Sidebar = () => {
 
   const links = [
     { name: 'Overview', href: '/Dashboard/accountOverview' },
-    { name: 'Billing History', href: '/Dashboard/Acount-biling-history' },
+    { name: 'Billing History', href: '/Dashboard/Account-biling-history' },
     { name: 'Security', href: '/security' },
-    { name: 'Privacy', href: '/privacy' },
+    { name: 'Privacy', href: '/Dashboard/AccountPrivicy' },
     { name: 'Log Out', href: '/logout' },
   ];
 
   return (
-    <div className="ml-12  p-4 mt-12 flex flex-col gap-2">
+    <div className="w-full md:ml-12 md:w-50 p-4 mt-4 md:mt-12 flex flex-col items-center md:items-start gap-2">
       {links.map((link) => (
-        <div key={link.name} className="flex justify-start">
+        <div key={link.name} className="w-full flex justify-center md:justify-start">
           <Link
             href={link.href}
-            className={`rounded-full px-4 py-2  transition-all  text-lg duration-200 ${
+            className={`rounded-full px-4 py-2 text-base md:text-lg transition-all duration-200 ${
               pathname === link.href
-                ? 'bg-gray-200 text-slate-600 text-lg font-bold'
-                : 'text-gray-600 hover:bg-gray-200 hover:text-slate-600  hover:leading-snug hover:font-bold'
+                ? 'bg-gray-200 text-slate-600 font-bold'
+                : 'text-gray-600 hover:bg-gray-200 hover:text-slate-600 hover:font-bold'
             }`}
           >
             {link.name}
           </Link>
-          
         </div>
       ))}
     </div>
